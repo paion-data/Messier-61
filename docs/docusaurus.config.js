5c884f6167873a4f7aff8cba8415eaadc24124f7
Jack <jack20191124@proton.me>
2023-02-02 16:32:01 +0800

    Fix doc build error

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -6,20 +6,17 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Messier-61',
+  tagline: 'Messier-61 Documentation',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
-  url: 'https://your-docusaurus-test-site.com',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  url: 'https://paion-data.github.io/',
+  baseUrl: '/Messier-61',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'paion-data',
+  projectName: 'Messier-61',
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
