5117534b9ec6a6d1a98bd8a5f5f02af024e3da20
Jack <jack20191124@proton.me>
2023-02-13 18:34:53 +0800

    Link home page to wiki page (#23)
    * Install router dependency
    
    * Add routing and push home page as a dedicated component behind router

---
+++ src/components/Home/Home.test.tsx
@@ -0,0 +1,13 @@
+// Copyright 2023 Paion Data. All rights reserved.
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "./Home";
+import { BrowserRouter } from "react-router-dom";
+
+test("renders app without error", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+});
--- src/components/Wiki/Editor.js
+++ src/components/Wiki/Editor.js
@@ -1,14 +1,14 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import React from "react";
-import ExampleTheme from "../themes/ExampleTheme";
+import ExampleTheme from "../../themes/ExampleTheme";
 import { LexicalComposer } from "@lexical/react/LexicalComposer";
 import { RichTextPlugin } from "@lexical/react/LexicalRichTextPlugin";
 import { ContentEditable } from "@lexical/react/LexicalContentEditable";
 import { HistoryPlugin } from "@lexical/react/LexicalHistoryPlugin";
 import { AutoFocusPlugin } from "@lexical/react/LexicalAutoFocusPlugin";
 import LexicalErrorBoundary from "@lexical/react/LexicalErrorBoundary";
-import TreeViewPlugin from "../plugins/TreeViewPlugin";
-import ToolbarPlugin from "../plugins/ToolbarPlugin";
+import TreeViewPlugin from "../../plugins/TreeViewPlugin";
+import ToolbarPlugin from "../../plugins/ToolbarPlugin";
 import { HeadingNode, QuoteNode } from "@lexical/rich-text";
 import { TableCellNode, TableNode, TableRowNode } from "@lexical/table";
 import { ListItemNode, ListNode } from "@lexical/list";
@@ -19,9 +19,9 @@ import { ListPlugin } from "@lexical/react/LexicalListPlugin";
 import { MarkdownShortcutPlugin } from "@lexical/react/LexicalMarkdownShortcutPlugin";
 import { TRANSFORMERS } from "@lexical/markdown";
 
-import ListMaxIndentLevelPlugin from "../plugins/ListMaxIndentLevelPlugin";
-import CodeHighlightPlugin from "../plugins/CodeHighlightPlugin";
-import AutoLinkPlugin from "../plugins/AutoLinkPlugin";
+import ListMaxIndentLevelPlugin from "../../plugins/ListMaxIndentLevelPlugin";
+import CodeHighlightPlugin from "../../plugins/CodeHighlightPlugin";
+import AutoLinkPlugin from "../../plugins/AutoLinkPlugin";
 
 function Placeholder() {
   return <div className="editor-placeholder">Enter some rich text...</div>;
