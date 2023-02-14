92ee390d5ae11e69f8a0d2bae7b24c2a92fc8915
Jack <jack20191124@proton.me>
2023-02-14 15:09:39 +0800

    Fix build error

--- src/components/Wiki/Editor.js
+++ src/components/Wiki/Editor.js
@@ -63,7 +63,6 @@ export default function Editor() {
           <HistoryPlugin />
           <TreeViewPlugin />
           <AutoFocusPlugin />
-          <CodeHighlightPlugin />
           <ListPlugin />
           <LinkPlugin />
           <AutoLinkPlugin />
