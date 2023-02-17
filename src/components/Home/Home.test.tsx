a21026f02c01a75fe1a357bd5b12883fac6e5a45
gongminghui <xxykx1016@168.com>
2023-02-17 10:14:53 +0800

    add link

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,9 +1,8 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "./Home";
 import { BrowserRouter } from "react-router-dom";
-import { screen } from "@testing-library/react";
 
 test("renders app without error", () => {
   render(
@@ -13,5 +12,16 @@ test("renders app without error", () => {
   );
 });
 
-const linkToTest = screen.getByRole("link", { name: /link to test/i });
-expect(linkToTest.getAttribute("href")).toMatchInlineSnapshot();
+test("GitHub and Help buttons point to the expected locations", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+
+  const githubLink = screen.getByRole("link", { name: /GitHub/i });
+  expect(githubLink.getAttribute("href")).toBe('https://github.com/paion-data/Messier-61');
+
+  const helpLink = screen.getByRole("link", { name: /Help/i });
+  expect(helpLink.getAttribute("href")).toBe('https://paion-data.github.io/Messier-61/');
+});
