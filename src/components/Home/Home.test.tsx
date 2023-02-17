3e891f8acd48b72df98bc70a44f353919844f1b1
Xxy1016 <125425805+Xxy1016@users.noreply.github.com>
2023-02-17 14:54:42 +0800

    Add link (#51)
    * add-link
    
    * add-link
    
    * add link
    
    * add link
    
    * add link
    
    * add link
    
    ---------
    
    Co-authored-by: mac <mac@macdeMacBook-Pro.local>
    Co-authored-by: gongminghui <xxykx1016@168.com>

--- src/components/Home/Home.test.tsx
+++ src/components/Home/Home.test.tsx
@@ -1,6 +1,6 @@
 // Copyright 2023 Paion Data. All rights reserved.
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Home from "./Home";
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,4 +10,18 @@ test("renders app without error", () => {
       <Home />
     </BrowserRouter>
   );
+});
+
+test("GitHub and Help buttons point to the expected locations", () => {
+  render(
+    <BrowserRouter>
+      <Home />
+    </BrowserRouter>
+  );
+
+  const githubLink = screen.getByRole("link", { name: /GitHub/i });
+  expect(githubLink.getAttribute("href")).toBe("https://github.com/paion-data/Messier-61");
+
+  const helpLink = screen.getByRole("link", { name: /Help/i });
+  expect(helpLink.getAttribute("href")).toBe("https://paion-data.github.io/Messier-61/");
 });
