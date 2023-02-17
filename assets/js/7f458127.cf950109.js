5c34d716759770b16e89d9f2ebac6ee8a4d98d71
QubitPi <jack20191124@proton.me>
2023-02-17 07:07:04 +0000

    deploy: 02eb36d31a573920d8ac5aac0c7fdffd8aa205d6

---
+++ assets/js/7f458127.cf950109.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[764],{9891:e=>{e.exports=JSON.parse('{"title":"Development","description":"Messier-61 Development Guide","slug":"/category/development","permalink":"/Messier-61/docs/category/development","navigation":{"next":{"title":"ci-cd","permalink":"/Messier-61/docs/development/ci-cd"}}}')}}]);
