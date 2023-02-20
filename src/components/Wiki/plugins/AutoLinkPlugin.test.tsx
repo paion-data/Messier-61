3df891729d2cc6cb0878fee5e81bf3de88911cae
gongminghui <xxykx1016@168.com>
2023-02-20 15:44:59 +0800

    test auto link

--- src/components/Wiki/plugins/AutoLinkPlugin.test.tsx
+++ src/components/Wiki/plugins/AutoLinkPlugin.test.tsx
@@ -5,6 +5,7 @@ import AutoLinkPlugin from "./AutoLinkPlugin";
 
 test("Converts a string to a url-link", () => {
   render(<AutoLinkPlugin />);
+  
   const urlify = (text: any) => {
     const urlRegex =
       /((https?:\/\/(www\.)?)|(www\.))[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
@@ -15,15 +16,4 @@ test("Converts a string to a url-link", () => {
   const text = "Find me at http://www.example.com and also at http://stackoverflow.com";
   const html = urlify(text);
   expect(html.getAttribute("href")).toBe("https://www.baidu.com/");
-
-  const isEmail = (emailText: any) => {
-    const emailRegex =
-      /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
-    return emailText.replace(emailRegex, (email: string) => {
-      return `<a href="${email}">${email}</a>`;
-    });
-  };
-  const emailText = "Find me at http://www.example.com and also at http://stackoverflow.com";
-  const emailHtml = isEmail(emailText);
-  expect(emailHtml.getAttribute("e-mail")).toBe("https://www.github.com/");
 });
