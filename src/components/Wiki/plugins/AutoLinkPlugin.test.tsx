9266238d99b868a9fd59107f6c3e2920f4183156
gongminghui <xxykx1016@168.com>
2023-02-20 15:59:34 +0800

    test auto link

--- src/components/Wiki/plugins/AutoLinkPlugin.test.tsx
+++ src/components/Wiki/plugins/AutoLinkPlugin.test.tsx
@@ -1,5 +1,5 @@
 // Copyright 2023 Paion Data. All rights reserved.
-
+import React from "react";
 import { render } from "@testing-library/react";
 import AutoLinkPlugin from "./AutoLinkPlugin";
 
@@ -20,4 +20,15 @@ test("Converts a string to a url-link", () => {
   const text = "Find me at http://www.example.com and also at http://stackoverflow.com";
   const html = urlify(text);
   expect(html.getAttribute("href")).toBe("https://www.baidu.com/");
+
+  const isEmail = (emailText: any) => {
+    const emailRegex =
+      /(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))/;
+    return emailText.replace(emailRegex, (email: string) => {
+      return `<a href="${email}">${email}</a>`;
+    });
+  };
+  const emailText = "Find me at http://www.example.com and also at http://stackoverflow.com";
+  const emailHtml = isEmail(emailText);
+  expect(emailHtml.getAttribute("e-mail")).toBe("https://www.github.com/");
 });
