ee297095cc98cdec08799900ba98e868bf8c86bc
Jack <jack20191124@proton.me>
2023-03-02 17:48:38 +0800

    Install TypeDoc dependencies in build

--- docs/scripts/typedoc.js
+++ docs/scripts/typedoc.js
@@ -2,29 +2,29 @@
 const TypeDoc = require("typedoc");
 
 async function main() {
-    const app = new TypeDoc.Application();
+  const app = new TypeDoc.Application();
 
-    // Ask TypeDoc to load tsconfig.json and typedoc.json files
-    app.options.addReader(new TypeDoc.TSConfigReader());
-    app.options.addReader(new TypeDoc.TypeDocReader());
+  // Ask TypeDoc to load tsconfig.json and typedoc.json files
+  app.options.addReader(new TypeDoc.TSConfigReader());
+  app.options.addReader(new TypeDoc.TypeDocReader());
 
-    app.bootstrap({
-        // typedoc options
-        entryPoints: ["../src/components/Graph/Graph.d.ts"],
-        tsconfig: "../tsconfig.json"
-    });
+  app.bootstrap({
+    // typedoc options
+    entryPoints: ["../src/components/Graph/Graph.d.ts"],
+    tsconfig: "../tsconfig.json",
+  });
 
-    const project = app.convert();
+  const project = app.convert();
 
-    // Project has converted correctly
-    if (project) {
-        const outputDir = "./build/api";
+  // Project has converted correctly
+  if (project) {
+    const outputDir = "./build/api";
 
-        // Rendered docs
-        await app.generateDocs(project, outputDir);
-        // Alternatively generate JSON output
-        await app.generateJson(project, outputDir + "/documentation.json");
-    }
+    // Rendered docs
+    await app.generateDocs(project, outputDir);
+    // Alternatively generate JSON output
+    await app.generateJson(project, outputDir + "/documentation.json");
+  }
 }
 
 main().catch(console.error);
