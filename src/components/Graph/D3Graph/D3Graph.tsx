8742b44d893f8a62f345e53fa4afa83696193a39
gongminghui <xxykx1016@163.com>
2023-03-03 10:56:00 +0800

    repair styles-Module

--- src/components/Graph/D3Graph/D3Graph.tsx
+++ src/components/Graph/D3Graph/D3Graph.tsx
@@ -39,8 +39,11 @@ export function D3Graph(props: Graph): JSX.Element {
 export function generateD3Graph(props: Graph): JSX.Element {
   const svgRef = React.useRef(null);
   const margin = { top: 10, right: 30, bottom: 30, left: 40 };
-  const width = styles.svgWidth - margin.left - margin.right;
-  const height = styles.svgHeight - margin.top - margin.bottom;
+  
+  const svgWidth = styles.svgWidth
+  const svgHeight = styles.svgHeight
+  const width = svgWidth - margin.left - margin.right;
+  const height = svgHeight - margin.top - margin.bottom;
   const svg = attachSvgTo(svgRef.current, margin, width, height);
 
   const link = initializeLinks(svg, props);
