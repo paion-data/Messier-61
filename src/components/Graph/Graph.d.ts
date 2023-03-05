169a2218cbf61d367b9160491a13e0759b595568
xue0108 <115383650+xue0108@users.noreply.github.com>
2023-03-05 16:33:42 +0800

    Implement real-time editor update (#60)
    Co-authored-by: Jack <jack20191124@proton.me>

--- src/components/Graph/Graph.d.ts
+++ src/components/Graph/Graph.d.ts
@@ -1,4 +1,5 @@
 // Copyright 2023 Paion Data. All rights reserved.
+
 /**
  * A Graph is an abstract data type that is meant to implement the Knowledge Graph (both directed and undirected)
  * concept from the field of graph theory within mathematics.
