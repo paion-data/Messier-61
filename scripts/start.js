b35636c45c3e13d90c4d39141ed162c20ce96c8b
Jack <jack20191124@proton.me>
2023-03-05 13:35:56 +0800

    Fix the 404 error on sub path refresh

--- scripts/start.js
+++ scripts/start.js
@@ -2,13 +2,14 @@
 "use strict";
 
 const configFactory = require("../config/webpack/webpack.config");
+const devServerConfig = require('../config/webpack/webpackDevServer.config');
 
 const Webpack = require("webpack");
 const WebpackDevServer = require("webpack-dev-server");
 const webpackConfig = configFactory("development");
 
 const compiler = Webpack(webpackConfig);
-const devServerOptions = { ...webpackConfig.devServer, open: true };
+const devServerOptions = { ...devServerConfig, open: true };
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 server.startCallback(() => {
